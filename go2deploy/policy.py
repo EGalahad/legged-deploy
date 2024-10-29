ad84b92df52eb0df12eda0e303d2d8153b3b14ba
btx0424 <btx0424@outlook.com>
2024-10-29 19:12:22 +0800

    update policy loading

--- go2deploy/policy.py
+++ go2deploy/policy.py
@@ -9,16 +9,18 @@ class ONNXModule:
     def __init__(self, path: str):
 
         self.ort_session = ort.InferenceSession(path, providers=["CPUExecutionProvider"])
-        self.meta = json.load(path.replace(".pt", ".json"), "r")
-        self.in_keys = self.meta["in_keys"]
-        self.out_keys = self.meta["out_keys"]
+        with open(path.replace(".onnx", ".json"), "r") as f:
+            self.meta = json.load(f)
+        self.in_keys = [k if isinstance(k, str) else tuple(k) for k in self.meta["in_keys"]]
+        self.out_keys = [k if isinstance(k, str) else tuple(k) for k in self.meta["out_keys"]]
     
     def __call__(self, input: Dict[str, np.ndarray]) -> Dict[str, np.ndarray]:
         args = {
-            inp.name: input[key] 
+            inp.name: input[key]
             for inp, key in zip(self.ort_session.get_inputs(), self.in_keys)
+            if key in input
         }
         outputs = self.ort_session.run(None, args)
-        outputs = {k: v for k, v in zip(self.meta["out_keys"], outputs)}
+        outputs = {k: v for k, v in zip(self.out_keys, outputs)}
         return outputs
     
